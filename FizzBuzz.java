2a3865f3b554d21882855dca75f3d8676724714c
Maya Driver <mayadrivermd@gmail.com>
2015-01-26 12:48:49 +0000

    passed second test

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,11 @@
 public class FizzBuzz {
 
   public boolean isDivisibleByThree(int n) {
-    if (3 % n == 0);
+    if (n % 3 == 0) {
       return true;
+    } else {
+      return false;
+    }
   }
 
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -5,9 +5,16 @@ public class FizzBuzzTest {
 
 @Test
 public void knowsThreeIsDivisibleByThree() {
-  System.out.println("Test if isDivisibleByThree returns true...");
+  System.out.println("Test if three isDivisibleByThree returns true...");
   FizzBuzz fizzbuzz = new FizzBuzz();
   assertTrue("3 is divisible by 3", fizzbuzz.isDivisibleByThree(3));
  }
+
+@Test
+public void knowsOneIsntDivisibleByThree() {
+  System.out.println("Test if one isDivisibleByThree returns false...");
+  FizzBuzz fizzbuzz = new FizzBuzz();
+  assertFalse("1 is divisible by 3", fizzbuzz.isDivisibleByThree(1));
+}
 
 }
